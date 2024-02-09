be5f25367f08c420499b734170615553588aab15
Rhehan Muzaky <rhehan7muzaky@gmail.com>
2024-02-10 02:10:24 +0700

    dart library

---
+++ example/hello.dart
@@ -0,0 +1,10 @@
+import 'package:belajar_dart_library/hello.dart'; // mengimport package library yang sudah kita buat, kita mengimportnya yg ada di dalam folder lib
+
+void main(){
+
+  print(sayHello("Rhehan"));
+
+  var customer = Customer();
+  var category = Category();
+  var product = Product();
+}
---
+++ lib/hello.dart
@@ -0,0 +1,4 @@
+library belajar_dart_library; // kata kunci library dengan namanya, disarankan namanya disamakan dengan nama project yang dibuat
+
+export 'src/say_hello.dart'; // mengeksport semua kode di file tersebut
+export 'src/customer.dart' show Customer, Category, Product; // mengeksport beberapa kode di file tersebut yg telah ditentukan
