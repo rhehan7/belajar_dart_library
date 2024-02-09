c62a17e78af5cb74abddc4233f21bd7f63bd56b3
Rhehan Muzaky <rhehan7muzaky@gmail.com>
2024-02-10 03:11:32 +0700

    update lib/hello.dart

--- lib/hello.dart
+++ lib/hello.dart
@@ -2,3 +2,7 @@ library belajar_dart_library; // kata kunci library dengan namanya, disarankan n
 
 export 'src/say_hello.dart'; // mengeksport semua kode di file tersebut
 export 'src/customer.dart' show Customer, Category, Product; // mengeksport beberapa kode di file tersebut yg telah ditentukan
+
+
+// NOTE:
+// ketika ingin mengimport suatu library, jangan mengimport bagian src nya, karena itu hanya untuk internal saja, rawan berubah, import yang berada di lib nya langsung seperti file hello.dart ini
